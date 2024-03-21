38bcad642caf0ed3ba2f5bb225a9f97d709587b9
geraldkisilu <muligerald6@gmail.com>
2024-03-21 18:19:54 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+var num1 = 2;
+var num2 = 31;
+let multiply = num1*num2;
+
+var random = Math.floor(Math.random()*4)+3;
+console.log(random);
+
+var num3 = 13;
+var num4 = 9;
+let mod = num3%num4;
+
+let max = Math.max(11,12,20);
+console.log(max);
