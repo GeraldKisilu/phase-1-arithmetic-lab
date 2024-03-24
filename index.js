7730b1ffa8df3d61a280832933cd8d5eaa26a082
geraldkisilu <muligerald6@gmail.com>
2024-03-24 21:56:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -4,13 +4,15 @@
 var num1 = 2;
 var num2 = 31;
 let multiply = num1*num2;
+console.log("Multiplication result:", multiply)
 
 var random = Math.floor(Math.random()*4)+3;
-console.log(random);
+console.log("random integer greater than 0:", random);
 
 var num3 = 13;
 var num4 = 9;
 let mod = num3%num4;
+console.log("Division of num 3 by num 4:", mod);
 
 let max = Math.max(11,12,20);
-console.log(max);
+console.log("Highest nuber in the set:", max);
